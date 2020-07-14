9ab67d24e8c358979d8cb8653247e3765963e5c6
Alexandru Cotin <a.cotin96@gmail.com>
2020-07-14 12:03:05 +0200

    Feat: Added submit button

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import {
+  View,
+} from "react-native";
 import AddEntry from "./components/AddEntry";
 
 export default function App() {
